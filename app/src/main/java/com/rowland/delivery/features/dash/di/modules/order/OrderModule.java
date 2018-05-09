4784f107861f9ced5206c734f38c671d741ce43c
RowlandOti <rowlandotienoo@gmail.com>
2018-05-10 01:41:42 +0300

    chore: add files

---
+++ app/src/main/java/com/rowland/delivery/features/dash/di/modules/order/OrderModule.java
@@ -0,0 +1,49 @@
+package com.rowland.delivery.features.dash.di.modules.order;
+
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.rowland.delivery.features.dash.data.model.order.OrderRepository;
+import com.rowland.delivery.features.dash.di.scope.order.Order;
+import com.rowland.delivery.features.dash.domain.usecases.order.LoadOrdersUseCase;
+import com.rowland.delivery.features.dash.presentation.adapters.OrderDataAdapter;
+import com.rowland.delivery.features.dash.presentation.viewmodels.order.OrderViewModelFactory;
+import com.rowland.delivery.services.firebase.modules.FirebaseModule;
+
+import javax.inject.Named;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Rowland on 5/9/2018.
+ */
+
+@Module(includes = {FirebaseModule.class})
+public class OrderModule {
+
+    @Provides
+    @Order
+    OrderDataAdapter providesOrderDataAdapter() {
+        return new OrderDataAdapter();
+    }
+
+    @Provides
+    @Order
+    @Named("order")
+    ViewModelProvider.Factory providesViewModelFactory(LoadOrdersUseCase loadOrdersUseCase) {
+        return new OrderViewModelFactory(loadOrdersUseCase);
+    }
+
+    @Provides
+    @Order
+    LoadOrdersUseCase providesLoadOrdersUseCase(OrderRepository OrderRepository) {
+        return new LoadOrdersUseCase(OrderRepository);
+    }
+
+    @Provides
+    @Order
+    OrderRepository providesOrderRepository(FirebaseFirestore firebaseFirestone) {
+        return new OrderRepository(firebaseFirestone);
+    }
+}
