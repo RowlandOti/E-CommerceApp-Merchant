a9ddb0a53de40abdde356ef30b053cf595d51e3e
Rowland Oti <rowlandotienoo@gmail.com>
2021-02-04 11:08:28 +0300

    Tooling upgrade (#4)
    * chore: upgrade build tools
    
    * chore: upgrade support, firebase lib versions
    
    * chore: upgrade rx firestore lib
    
    * refactor: done some code refactor
    
    * fix: clear old list in case category changes
    
    * fix: use a more determinate Toolbar color as default.
    
    * fix: rectified problem of auth textviews being cut-off to the right
    
    * refactor: remove unneeded impl for Completeable
    
    * chore: avoid magic numbers
    
    * fix: use Kotlin round func
    
    * fix: use Kotlin round func
    
    * chore: make const as recommended
    
    * format: style code
    
    * chore: write some tests for the ath activity
    
    Co-authored-by: Otieno Rowland <r.otieno@kokonetworks.com>

---
+++ ui/src/androidTest/java/com/rowland/delivery/merchant/TestUtil.kt
@@ -0,0 +1,16 @@
+package com.rowland.delivery.merchant
+
+import androidx.test.InstrumentationRegistry.getContext
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.rowland.delivery.merchant.robots.LoginRobot
+
+/**
+ * Created by rowlandoti on 31/01/2021
+ *
+ */
+
+fun login(func: LoginRobot.() -> Unit) = LoginRobot()
+    .apply { func() }
+
+fun getString(resId: Int) = InstrumentationRegistry.getInstrumentation().targetContext.resources.getString(resId)
--- ui/src/androidTest/java/com/rowland/delivery/merchant/features/auth/ui/AuthActivityTest.kt
+++ ui/src/androidTest/java/com/rowland/delivery/merchant/features/auth/ui/AuthActivityTest.kt
@@ -1,48 +1,79 @@
 package com.rowland.delivery.merchant.features.auth.ui
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
+import com.google.firebase.auth.FirebaseAuth
 import com.rowland.delivery.merchant.R
+import com.rowland.delivery.merchant.getString
+import com.rowland.delivery.merchant.login
 import com.squareup.spoon.SpoonRule
-import org.junit.Rule
-import org.junit.Test
-
+import org.junit.*
 
 class AuthActivityTest {
 
     @get:Rule
     val spoon = SpoonRule()
+
     @get:Rule
     val authActivityRule: ActivityTestRule<AuthActivity> = ActivityTestRule(AuthActivity::class.java)
 
-    @Test
-    fun onCreate() {
-        spoon.screenshot(authActivityRule.getActivity(), "auth_screen");
+    @After
+    fun tearDown() {
 
-        Espresso.onView(withId(R.id.input_email)).perform(replaceText(USER_EMAIL));
-        Espresso.onView(withId(R.id.input_password)).perform(replaceText(USER_PASS));
-        Espresso.onView(withId(R.id.btn_login)).perform(click())
-
-        spoon.screenshot(authActivityRule.getActivity(), "auth_login");
     }
 
-   /* @Test
-    fun onActivityResult() {
+    @Test
+    fun onLoginMissingEmailAndPasswordFails() {
+        login {
+            clickLogin()
+            spoon.screenshot(authActivityRule.activity, "auth_login_missing_credentials");
+            matchToastText(getString(R.string.login_unsuccessful), authActivityRule.activity.window.decorView)
+        }
+    }
+
+    @Test
+    fun onLoginMissingEmailFails() {
+        login {
+            setPassword("1234")
+            clickLogin()
+            spoon.screenshot(authActivityRule.activity, "auth_login_missing_email");
+            matchToastText(getString(R.string.login_unsuccessful), authActivityRule.activity.window.decorView)
+        }
+    }
+
+    @Test
+    fun onLoginMissingPasswordFails() {
+        login {
+            setEmail("mail@example.com")
+            clickLogin()
+            spoon.screenshot(authActivityRule.activity, "auth_login_missing_password");
+            matchToastText(getString(R.string.login_unsuccessful), authActivityRule.activity.window.decorView)
+        }
+    }
+
+    @Test
+    fun onLoginWrongPasswordFails() {
+        login {
+            setEmail("mail@example.com")
+            setPassword("wrong")
+            clickLogin()
+            spoon.screenshot(authActivityRule.activity, "auth_login_wrong_password");
+            matchToastText(getString(R.string.login_successful), authActivityRule.activity.window.decorView)
+        }
     }
 
     @Test
     fun onLoginSuccess() {
+        login {
+            setEmail(USER_EMAIL)
+            setPassword(USER_PASS)
+            clickLogin()
+            spoon.screenshot(authActivityRule.activity, "auth_login_success");
+            matchToastText(getString(R.string.login_successful), authActivityRule.activity.window.decorView)
+        }
     }
 
-    @Test
-    fun onLoginFailure() {
-
-    }*/
-
     companion object {
+
         val USER_EMAIL = "test@delivery.com"
         val USER_PASS = "qsWercjdGg"
     }
