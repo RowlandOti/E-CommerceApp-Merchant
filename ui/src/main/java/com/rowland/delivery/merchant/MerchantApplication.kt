57ed267260c4099ae5b3611747b5b53aaa3000eb
Otieno Rowland <r.otieno@kokonetworks.com>
2021-02-28 23:29:06 +0300

    chore: supoort rxjava 2x

--- ui/src/main/java/com/rowland/delivery/merchant/MerchantApplication.kt
+++ ui/src/main/java/com/rowland/delivery/merchant/MerchantApplication.kt
@@ -3,6 +3,7 @@ package com.rowland.delivery.merchant
 import androidx.multidex.MultiDexApplication
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.HiltAndroidApp
+import io.reactivex.plugins.RxJavaPlugins
 
 /**
  * Created by Rowland on 4/30/2018.
@@ -13,6 +14,7 @@ class MerchantApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        RxJavaPlugins.setErrorHandler { e -> }
         FirebaseFirestore.setLoggingEnabled(BuildConfig.DEBUG)
     }
 }
