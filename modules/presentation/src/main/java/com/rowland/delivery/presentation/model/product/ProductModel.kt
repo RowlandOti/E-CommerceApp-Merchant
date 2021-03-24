830ede14fcaf5b51e44f17ab591031dfc7adf608
Otieno Rowland <r.otieno@kokonetworks.com>
2021-03-24 11:58:20 +0300

    chore: applied licence file

--- modules/presentation/src/main/java/com/rowland/delivery/presentation/model/product/ProductModel.kt
+++ modules/presentation/src/main/java/com/rowland/delivery/presentation/model/product/ProductModel.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Otieno Rowland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rowland.delivery.presentation.model.product
 
 import android.os.Parcelable
@@ -26,6 +42,9 @@ class ProductModel : Parcelable {
     var firestoreUid: String? = null
 
     override fun toString(): String {
-        return "ProductModel(id=$id, name=$name, description=$description, price=$price, itemCode=$itemCode, merchantCode=$merchantCode, itemQuantity=$itemQuantity, unitsOfMeasure=$unitsOfMeasure, saleQuantity=$saleQuantity, imageUrl=$imageUrl, imageUrls=$imageUrls, createdAt=$createdAt, updatedAt=$updatedAt, deletedAt=$deletedAt, firestoreUid=$firestoreUid)"
+        return "ProductModel(id=$id, name=$name, description=$description, price=$price, itemCode=$itemCode, " +
+            "merchantCode=$merchantCode, itemQuantity=$itemQuantity, unitsOfMeasure=$unitsOfMeasure, " +
+            "saleQuantity=$saleQuantity, imageUrl=$imageUrl, imageUrls=$imageUrls, createdAt=$createdAt, " +
+            "updatedAt=$updatedAt, deletedAt=$deletedAt, firestoreUid=$firestoreUid)"
     }
 }
