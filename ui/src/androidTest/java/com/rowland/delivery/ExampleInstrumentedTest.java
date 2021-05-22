fec865774c697b10298ff8b37d563a9883fe04a3
Otieno Rowland <r.otieno@kokonetworks.com>
2021-05-22 12:08:30 +0300

    test: update tests dependencies

--- ui/src/androidTest/java/com/rowland/delivery/ExampleInstrumentedTest.java
+++ ui/src/androidTest/java/com/rowland/delivery/ExampleInstrumentedTest.java
@@ -3,8 +3,8 @@ package com.rowland.delivery;
 import static org.junit.Assert.*;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.*;
 import org.junit.runner.*;
 
@@ -15,11 +15,12 @@ import org.junit.runner.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
-        assertEquals("com.rowland.delivery", appContext.getPackageName());
+        assertEquals("com.rowland.delivery.merchant", appContext.getPackageName());
     }
 }
